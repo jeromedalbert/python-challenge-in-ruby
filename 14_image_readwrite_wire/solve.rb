f2d3d7dd4f2adcd31c1ba4e75df6552cfae2de9b
Jerome Dalbert <jerome.dalbert@gmail.com>
2013-04-14 19:15:07 +0200

    Refactor : level 14 better and shorter algorithm

--- 14_image_readwrite_wire/solve.rb
+++ 14_image_readwrite_wire/solve.rb
@@ -1,60 +1,32 @@
 # Uses RMagick gem
 require 'RMagick'
 include Magick
-  
-module Magick
-  class Image
-    alias_method :width, :columns
-  end
-end
 
-Point = Struct.new(:x, :y)
-
-Incr = { left: [-1, 0], right: [1, 0],
-         up:   [0, -1], down:  [0, 1] }
-
-def draw_lines(img_in, img_out)
-  img_in_x = 0
-  pos = Point.new(0, 0)
-  line_length = img_out.width
-
-  while img_in_x < img_in.width
-    img_in_x += draw_line(pos, :right, line_length, img_in, img_in_x, img_out)
-    pos = Point.new(pos.x + line_length - 1, pos.y + 1)
-    line_length -= 1        
-    
-    img_in_x += draw_line(pos, :down,  line_length, img_in, img_in_x, img_out)
-    pos = Point.new(pos.x - 1, pos.y + line_length - 1)
-    
-    img_in_x += draw_line(pos, :left, line_length, img_in, img_in_x, img_out)
-    pos = Point.new(pos.x - line_length + 1, pos.y - 1)
-    line_length -= 1
-    
-    img_in_x += draw_line(pos, :up, line_length, img_in, img_in_x, img_out)
-    pos = Point.new(pos.x + 1, pos.y - line_length + 1)
-  end
-end
-
-def draw_line(pos, dir, line_length, img_in, offset, img_out)      
-  line_length.times do |i|
-    color = img_in.pixel_color(offset + i, 0)
-    
-    img_out.pixel_color(pos.x + i * Incr[dir].first,
-                        pos.y + i * Incr[dir].last,
-                        color)
-  end
-  
-  line_length
-end
-
-
+Directions = [ [:right, 1, 0], [:down, 0, 1], [:left, -1, 0], [:up, 0, -1] ]
 
 
 img_in = Image.read("wire.png").first
+img_out = Image.new(100, 100)
 
-side_length = Math.sqrt(img_in.width)
-img_out = Image.new(side_length, side_length)
+line_length = 100
+img_in_index = 0
+x, y = [-1, 0]
 
-draw_lines(img_in, img_out)
+while line_length > 0
+  
+  Directions.each do |direction, dx, dy|    
+    line_length.times do
+      x += dx
+      y += dy
+      
+      img_out.pixel_color(x, y, img_in.pixel_color(img_in_index, 0))
+    
+      img_in_index += 1
+    end
+    
+    line_length -= 1 if direction == :right || direction == :left
+  end 
+  
+end
 
 img_out.write("output.jpg")
